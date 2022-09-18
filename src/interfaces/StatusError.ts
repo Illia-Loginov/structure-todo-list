dcff07990bc4a3d6a84c90eda9867ab4d7646178
Illia-Loginov <illia.loginov.g@gmail.com>
2022-09-18 22:24:24 +0300

    Add error templates

--- src/interfaces/StatusError.ts
+++ src/interfaces/StatusError.ts
@@ -1,3 +1,8 @@
-export interface StatusError extends Error {
-    statusCode: number
+export class StatusError extends Error {
+    statusCode: number;
+    
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+    }
 }
---
+++ src/utils/errors.ts
@@ -0,0 +1,11 @@
+import { StatusError } from "../interfaces";
+
+export const badRequest = (message: string): StatusError => {
+    const error: StatusError = new StatusError(message, 400);
+    return error;
+}
+
+export const notFound = (message: string): StatusError => {
+    const error: StatusError = new StatusError(message, 404);
+    return error;
+}
