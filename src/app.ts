ff069a405b4214627ed93cb77a4fa1a316456193
Illia-Loginov <illia.loginov.g@gmail.com>
2022-09-18 22:11:22 +0300

    Add default error handler

--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,12 @@ app.get('/', (req, res, next) => {
     res.send('Hello World!');
 })
 
+app.use((error: Error | StatusError, req: Request, res: Response, next: NextFunction) => {
+    if('statusCode' in error) {
+        res.status(error.statusCode).json(error.message);
+    } else {
+        res.status(500).json(error.message);
+    }
+})
+
 export default app;
