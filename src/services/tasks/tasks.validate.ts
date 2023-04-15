21d8cf5ffce66e9c856ff25b3299f444678b7dfd
Illia Loginov <illia.loginov.g@gmail.com>
2023-04-16 00:36:23 +0300

    Move validation schemas outside of functions in services/tasks/tasks.validate.ts

--- src/services/tasks/tasks.validate.ts
+++ src/services/tasks/tasks.validate.ts
@@ -1,53 +1,53 @@
 import Joi from 'joi';
-import { Task } from '../../interfaces';
+import { ITask } from '../../interfaces';
 
-export const validateCreateOne = (payload: any): Promise<Task> => {
-  const schema = Joi.object({
-    body: Joi.string().min(1).max(200).required(),
-    deadline: Joi.date().greater('now').optional()
-  });
+const dateRange = Joi.object({
+  start: Joi.date().optional(),
+  end: Joi.date().optional()
+});
 
-  return schema.validateAsync(payload);
+const createOneSchema = Joi.object({
+  body: Joi.string().min(1).max(200).required(),
+  deadline: Joi.date().greater('now').optional()
+});
+
+const getManySchema = Joi.object({
+  sort: Joi.object({
+    createdAt: Joi.string().valid('asc', 'desc').optional(),
+    deadline: Joi.string().valid('asc', 'desc').optional(),
+    completed: Joi.string().valid('asc', 'desc').optional()
+  }).optional(),
+  filter: Joi.object({
+    body: Joi.string().min(0).max(200).optional(),
+    createdAt: dateRange.optional(),
+    deadline: dateRange.optional(),
+    completed: dateRange.optional()
+  }).optional(),
+  skip: Joi.number().min(0).optional(),
+  limit: Joi.number().min(0).optional()
+});
+
+const taskIdSchema = Joi.object({
+  taskId: Joi.string().hex().length(24).required()
+});
+
+const updateOneSchema = Joi.object({
+  body: Joi.string().min(1).max(200).optional(),
+  deadline: Joi.date().greater('now').optional()
+});
+
+export const validateCreateOne = (payload: any): Promise<ITask> => {
+  return createOneSchema.validateAsync(payload);
 };
 
 export const validateGetMany = (payload: any) => {
-  const dateInterval = Joi.object({
-    start: Joi.date().optional(),
-    end: Joi.date().optional()
-  });
-
-  const schema = Joi.object({
-    sort: Joi.object({
-      createdAt: Joi.string().valid('asc', 'desc').optional(),
-      deadline: Joi.string().valid('asc', 'desc').optional(),
-      completed: Joi.string().valid('asc', 'desc').optional()
-    }).optional(),
-    filter: Joi.object({
-      body: Joi.string().min(0).max(200).optional(),
-      createdAt: dateInterval.optional(),
-      deadline: dateInterval.optional(),
-      completed: dateInterval.optional()
-    }).optional(),
-    skip: Joi.number().min(0).optional(),
-    limit: Joi.number().min(0).optional()
-  });
-
-  return schema.validateAsync(payload);
+  return getManySchema.validateAsync(payload);
 };
 
 export const validateTaskId = (params: any): Promise<{ taskId: string }> => {
-  const schema = Joi.object({
-    taskId: Joi.string().hex().length(24).required()
-  });
-
-  return schema.validateAsync(params);
+  return taskIdSchema.validateAsync(params);
 };
 
-export const validateUpdateOne = (payload: any): Promise<Partial<Task>> => {
-  const schema = Joi.object({
-    body: Joi.string().min(1).max(200).optional(),
-    deadline: Joi.date().greater('now').optional()
-  });
-
-  return schema.validateAsync(payload);
+export const validateUpdateOne = (payload: any): Promise<Partial<ITask>> => {
+  return updateOneSchema.validateAsync(payload);
 };
